ba0bf190954e99975634f3a77eee00c2bac9dcef
tzs186 <978212484@qq.com>
2019-09-11 16:14:14 +0800

    第一次提交

--- src/page/live/live_detail.vue
+++ src/page/live/live_detail.vue
@@ -2,9 +2,9 @@
   <div class="">
     <heads :openId="opens"></heads>
     <div class="live_deatail">
-      <div class=""></div>
+
     </div>
-    <div  class="prism-player" id="J_prismPlayer"></div>
+
 
     <foot></foot>
   </div>
@@ -19,34 +19,6 @@
                 opens: "sad",
 
             }
-        },
-        mounted(){
-            var player = new Aliplayer({
-                id: 'J_prismPlayer',
-                width: '100%',
-                autoplay: true,
-                //支持播放地址播放,此播放优先级最高
-                source : 'http://pull.ymgxjy.com/ymjy/58.flv?auth_key=1568256738000-0-0-4651a7ab7a77cbd95feaae2e93876d66',
-                //播放方式二：点播用户推荐
-                vid : '470b4d64c0dd3d67740d8afa',
-                playauth : 'ddd',
-                cover: 'http://liveroom-img.oss-cn-qingdao.aliyuncs.com/logo.png',
-                //播放方式三：仅MPS用户使用
-                vid : '470b4d64c0dd3d67740d8afa',
-                accId: 'dd',
-                accSecret: 'dd',
-                stsToken: 'dd',
-                domainRegion: 'dd',
-                authInfo: 'dd',
-                //播放方式四：使用STS方式播放
-                vid : '470b4d64c0dd3d67740d8afa',
-                accessKeyId: 'dd',
-                securityToken: 'dd',
-                accessKeySecret: 'dd',
-                region:'cn-shanghai',//eu-central-1,ap-southeast-1
-            },function(player){
-                console.log('播放器创建好了。')
-            });
         }
     }
 </script>
